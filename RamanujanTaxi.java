b18cfebe049c8defdfab0d1d22b6a839d5f2445f
jthomasASC <thomasjordane7@gmail.com>
2016-10-07 04:55:36 -0400

    File types to Ignore

---
+++ RamanujanTaxi.java
@@ -0,0 +1,13 @@
+class RamanujanTaxi{
+  
+  /*
+   * take in a number out put the closest Hardy-Ramanujan number
+   */
+  
+  public static main(String [] args){
+    int [] pairs = new int [4];//[0] and [1] are a set and [2] and [3] are a set
+    int upperBound = args[0];
+    int 
+    
+  }
+}
