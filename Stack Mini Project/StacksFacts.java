5bd62d9ba712532a0c5f0a8f756725e77007a8a7
jthomasASC <thomasjordane7@gmail.com>
2017-02-27 10:13:34 -0500

    Comp Sci Home Work

---
+++ Stack Mini Project/StacksFacts.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Stack;
+
+/*
+ * made by Jordane Thomas
+ * Takes in strings from the command line, as an array, and spites them out in reverse order.
+ */
+public class StacksFacts {
+
+	public static void main(String[] args) {
+
+		Stack<String> stc = new Stack<String>();
+		stc.addAll(Arrays.asList(args));
+
+		while (stc.size() > 0)
+			System.out.print(stc.pop() + " ");
+		System.out.print("\n");
+	}
+
+}
