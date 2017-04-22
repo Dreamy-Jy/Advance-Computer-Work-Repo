911847ba691c74051f8523ee87c48530b94966b9
jthomasASC <thomasjordane7@gmail.com>
2017-04-21 21:29:03 -0400

    Basic GUI Layout update

--- Java GUI temp Converter/Converter.java
+++ Java GUI temp Converter/Converter.java
@@ -2,32 +2,74 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-class Converter extends JFrame{
+class Converter extends JFrame {
 
-    JPanel panel = new JPanel();
-    JButton button = new JButton("Convert");
+	JPanel panel = new JPanel();
 
-    public static void main (String [] args){
-        new Converter();
-    }
+	JButton FButton = new JButton("Convert>>>");
+	JTextField FText = new JTextField("", 10);
+	JLabel FLabel = new JLabel("Fahrenheit");
 
-    public Converter(){
-        super("Temperature Converter");
-        setSize(300, 300);
-        setResizable(true);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+	JButton CButton = new JButton("<<<<Convert");
+	JTextField CText = new JTextField("", 10);
+	JLabel CLabel = new JLabel("Celsius");
 
-        
-        button.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent event){
-                System.out.println("hello");
-            }
-        });
-        //panel
-        panel.add(button);
-        
-        add(panel);
+	public static void main(String[] args) {
+		new Converter();
+	}
 
-        setVisible(true);
-    }
+	public Converter() {
+		super("Temperature Converter");
+		setSize(300, 300);
+		setResizable(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		FButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				try {
+					double output = convertToC(Double.parseDouble(FText.getText()));
+					System.out.println(output);
+					FText.setText("");
+					CText.setText(output+"");
+				} catch (Exception e) {
+					System.out.print("enter a number");
+				}
+			}
+		});
+		CButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				try {
+					double output = convertToF(Double.parseDouble(CText.getText()));
+					System.out.println(output);
+					CText.setText("");
+					FText.setText(output+"");
+				} catch (Exception e) {
+					System.out.print("enter a number");
+				}
+			}
+
+		});
+		// panel
+		panel.add(FLabel);
+		panel.add(FText);
+		panel.add(FButton);
+		panel.add(CLabel);
+		panel.add(CText);
+		panel.add(CButton);
+
+		add(panel);
+
+		setVisible(true);
+	}
+
+	public double convertToC(double f) {
+		return ((f - 32) * 5) / 9;
+	}
+	private double convertToF(double c) {
+		return 32 + (c * 9 / 5);
+	}
 }
